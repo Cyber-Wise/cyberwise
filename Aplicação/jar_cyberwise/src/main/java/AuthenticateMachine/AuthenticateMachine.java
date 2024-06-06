8e5671d797def102a7bb6ba319c42e4ab4c9b232
Amanda-Raim <amanda.raim@sptech.school>
2024-06-05 21:17:33 -0300

    máquina e hostname atualizados na chamada

--- Aplicação/jar_cyberwise/src/main/java/AuthenticateMachine/AuthenticateMachine.java
+++ Aplicação/jar_cyberwise/src/main/java/AuthenticateMachine/AuthenticateMachine.java
@@ -1,43 +1,38 @@
 package AuthenticateMachine;
-import CaptureData.CaptureData;
+
 import CaptureData.CaptureDataRegister;
 import Connection.ConnectionLocal;
 import Connection.ConnectionServer;
 import com.github.britooo.looca.api.core.Looca;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.util.List;
 
-public class AuthenticateMachine{
-    public static Machine authenticateMachine(Integer codigoAcesso){
-     Machine machine = new Machine();
+public class AuthenticateMachine {
+    public static Machine authenticateMachine(Integer codigoAcesso) {
+        ConnectionServer connectionServer = new ConnectionServer();
+        ConnectionLocal connectionLocal = new ConnectionLocal();
+        JdbcTemplate con = connectionServer.getConexaoDoBanco();
+        Looca looca = new Looca();
+        String idProcessador = looca.getProcessador().getId();
 
-    ConnectionServer connectionServer = new ConnectionServer();
-    ConnectionLocal connectionLocal = new ConnectionLocal();
-    JdbcTemplate con = connectionServer.getConexaoDoBanco();
-    Looca looca = new Looca();
-    String idPrcessador = looca.getProcessador().getId();
+        List<Machine> response = con.query(
+                "SELECT id, modelo, hostname, NumeroSerieProcessador FROM maquina WHERE codigoAcesso = ?",
+                new BeanPropertyRowMapper<>(Machine.class), codigoAcesso);
 
-    List<Machine> response = con.query("SELECT id, modelo, hostname, NumeroSerieProcessador FROM maquina WHERE codigoAcesso = " + codigoAcesso + ";"
-            , new BeanPropertyRowMapper<>(Machine.class));
-
-    if(response.isEmpty()) {
-        System.out.println("Não foi possível encontrar sua máquina, verifique o código de acesso.");
-        return null;
-    } else {
-
-        if(response.get(0).getHostname() == null){
-            CaptureDataRegister.RegistrarDados(response.get(0).getId());
+        if (response.isEmpty()) {
+            System.out.println("Não foi possível encontrar sua máquina, verifique o código de acesso.");
+            return null;
+        } else {
+            Machine machine = response.get(0);
+            if (machine.getHostname() == null) {
+                CaptureDataRegister.RegistrarDados(machine.getId());
+            } else if (machine.getNumeroSerieProcessador().equalsIgnoreCase(idProcessador)) {
+                return machine;
+            }
+            System.out.println("Esse código de acesso não pertence a esta máquina.");
+            return null;
         }
-        else if(response.get(0).getNumeroSerieProcessador().equalsIgnoreCase(idPrcessador)){
-        machine.setId(response.get(0).getId());
-        return machine;
-
-        }
-        System.out.println("Esse código de acesso não pertence a esta máquina.");
-        return null;
-        }
-
     }
-    }
-
+}
